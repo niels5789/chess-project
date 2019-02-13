ec0af750df7d25bcbc00818ccade31dad8e18564
Suttorp <jeroen.suttorp@gmail.com>
2019-02-13 13:58:14 +0100

    Front-end

--- Front-end/chessproject/src/app/board-service.service.ts
+++ Front-end/chessproject/src/app/board-service.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import {Piece} from '../Piece';
+import {Tile} from '../Tile';
+import {buildPath} from 'selenium-webdriver/http';
 
 @Injectable()
 export class BoardServiceService {
-
+private tileURL = 'http://localhost:8080/';
   constructor(private http: HttpClient) {
   }
 
 
-  findAll(): Observable<Piece[]> {
+  findAll(): Observable<Tile[]> {
     return this.http.get<any>('http://localhost:8080/newboard').pipe(
-      catchError(this.handleError<Piece>(`findAll`))
+      catchError(this.handleError<Tile>(`findAll`))
     );
+  }
+
+  saveTile(id1: number, id2: number) {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+    };
+        return this.http.put(`${this.tileURL}/board/${id1}/${id2}`, httpOptions).pipe();
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
