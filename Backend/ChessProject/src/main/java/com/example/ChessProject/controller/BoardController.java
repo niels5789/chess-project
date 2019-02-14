f888aec6bf98f80ba037d731fa9d7948cbc36d7c
niels5789 <ferguson.niels@gmail.com>
2019-02-14 13:47:15 +0100

    added several features for piece movements: - double step pawn. - pieces can only take other colored pieces - pawn takes diagonal

--- Backend/ChessProject/src/main/java/com/example/ChessProject/controller/BoardController.java
+++ Backend/ChessProject/src/main/java/com/example/ChessProject/controller/BoardController.java
@@ -29,25 +29,39 @@ public class BoardController {
         String Piece = Tilelist.get(idvan).getName();
         int x1 = -1, y1 = -1, x2 = -1, y2 = -1;
 
-//        This value should be set to false if done testing
-        boolean validMove = true;
-
-        // get coordinates
-        x1 = getX(idvan, Tilelist);
-        y1 = getY(idvan, Tilelist);
-        x2 = getX(idnaar, Tilelist);
-        y2 = getY(idnaar, Tilelist);
-
+        boolean validMove = false;
 
 //      check if the move is legal
-        switch (Piece){
-            case "Pawn": validMove = validPawnMove(x1, y1, x2, y2, idvan, idnaar, Tilelist); break;
-            case "Rook": validMove = validRookMove(x1, y1, x2, y2, idvan, idnaar, Tilelist); break;
-            case "Knight": validMove = validKnightMove(x1, y1, x2, y2, idvan, idnaar, Tilelist); break;
-            case "Bishop": validMove = validBishopMove(x1, y1, x2, y2, idvan, idnaar, Tilelist); break;
-            case "Queen": validMove = validQueenMove(x1, y1, x2, y2, idvan, idnaar, Tilelist); break;
-            case "King": validMove = validKingMove(x1, y1, x2, y2, idvan, idnaar, Tilelist); break;
-            default: break;
+        if( idvan != idnaar && (Tilelist.get(idvan).getColor() != Tilelist.get(idnaar).getColor() || Tilelist.get(idnaar).getName().equals(""))) {
+
+            // get coordinates
+            x1 = getX(idvan, Tilelist);
+            y1 = getY(idvan, Tilelist);
+            x2 = getX(idnaar, Tilelist);
+            y2 = getY(idnaar, Tilelist);
+
+            switch (Piece) {
+                case "Pawn":
+                    validMove = validPawnMove(x1, y1, x2, y2, idvan, idnaar, Tilelist);
+                    break;
+                case "Rook":
+                    validMove = validRookMove(x1, y1, x2, y2, idvan, idnaar, Tilelist);
+                    break;
+                case "Knight":
+                    validMove = validKnightMove(x1, y1, x2, y2, idvan, idnaar, Tilelist);
+                    break;
+                case "Bishop":
+                    validMove = validBishopMove(x1, y1, x2, y2, idvan, idnaar, Tilelist);
+                    break;
+                case "Queen":
+                    validMove = validQueenMove(x1, y1, x2, y2, idvan, idnaar, Tilelist);
+                    break;
+                case "King":
+                    validMove = validKingMove(x1, y1, x2, y2, idvan, idnaar, Tilelist);
+                    break;
+                default:
+                    break;
+            }
         }
 
         if (validMove) {
@@ -124,8 +138,22 @@ public class BoardController {
         boolean valid = false;
         int color = list.get(idvan).getColor();
 
-        if((color == 0 && (x1 == x2 && y1 + 1 == y2))||(color == 1 && (x1 == x2 && y1 - 1 == y2))) {
-            valid = true;
+
+        if(list.get(idnaar).getName().equals("")) {
+            // basic 1 step move
+            if ((color == 0 && (x1 == x2 && y1 + 1 == y2)) || (color == 1 && (x1 == x2 && y1 - 1 == y2))) {
+                valid = true;
+            }
+
+            // double step move
+            if ((color == 0 && (x1 == x2 && y1 + 2 == y2 && y1 == 2)) || (color == 1 && (x1 == x2 && y1 - 2 == y2 && y1 == 7))) {
+                valid = true;
+            }
+        } else {
+            if (color == 0 && ((y2 == y1 + 1)&&( x2 == x1 - 1|| x2 == x1 + 1 )) || color == 1 && ((y2 == y1 - 1) && ( x2 == x1 - 1|| x2 == x1 + 1 ))){
+                valid = true;
+            }
+
         }
 
         return valid;
